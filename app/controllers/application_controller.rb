021b4bdd939af2c0d4d0a9d775ab60a10f133320
Frcomparan <frcomparan@gmail.com>
2022-09-02 07:53:55 -0500

    Fix bug in sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  
   before_action :set_locale
 
   def set_locale
     I18n.locale = 'es'
+  end
+
+  protected
+  def after_sign_in_path_for(resource)
+    root_path
   end
 end
